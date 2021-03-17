15a7b2bff2054d4b5e2924168fd3480a71f8b7fa
abdulateef <thepen0411@gmail.com>
2021-03-17 06:29:21 -0700

    initial commit

---
+++ LMS/Payload.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS
+{
+    public class Payload
+    {
+        public string  Country { get; set; }
+    }
+}
---
+++ LMS/WeatherForecast.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace LMS
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+
+        static double[] runningMedian(int[] a)
+        {
+            int[] sortedList = new int[a.Length];
+            Array.Sort(a);
+            if (a.Length == 1)
+            {
+
+            }
+        }
+
+    }
+}
